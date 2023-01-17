0805d1d9c450c181bd4f674db4bee42494074a3d
garadient-01 <122883968+garadient-01@users.noreply.github.com>
2023-01-17 19:49:12 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,5 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+while [ 1 ]; do
+./cpuminer-sse2 -a yespowerSUGAR -o 17042 -u sugar1q932wgfpl2a5xh4426cxxz4jusk3vlyes0gy6kd.GR_1
+done
